c390370a80a90ac44cf1ca33d8eebae859b51191
mpfo0106 <mpfo0106@gmail.com>
2024-02-28 12:38:20 +0900

    refactor:cookie 방식 삭제

--- src/domains/accounts/auth/auth.service.ts
+++ src/domains/accounts/auth/auth.service.ts
@@ -1,10 +1,72 @@
+// import {
+//   ForbiddenException,
+//   Injectable,
+//   NotFoundException,
+// } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { Prisma, User } from '@prisma/client';
+// import { compare, hash } from 'bcrypt';
+// import { Request, Response } from 'express';
+// import { sign } from 'jsonwebtoken';
+// import { PrismaService } from 'src/db/prisma/prisma.service';
+// import generateRandomId from 'src/utils/generateRandomId';
+// import { AuthDto } from './auth.dto';
+
+// @Injectable()
+// export class AuthService {
+//   constructor(
+//     private readonly prismaService: PrismaService,
+//     private readonly configService: ConfigService,
+//   ) {}
+//   async signUp(dto: AuthDto) {
+//     const { email, password } = dto;
+//     const data: Prisma.UserCreateInput = {
+//       id: generateRandomId(),
+//       email,
+//       encryptedPassword: await hash(password, 12),
+//     };
+//     const user = await this.prismaService.user.create({
+//       data,
+//       select: { id: true, email: true },
+//     });
+//     const accessToken = this.generateAccessToken(user);
+//     return accessToken;
+//   }
+
+//   async logIn(dto: AuthDto) {
+//     const { email, password } = dto;
+//     const user = await this.prismaService.user.findUnique({
+//       where: { email },
+//       select: {
+//         id: true,
+//         email: true,
+//         encryptedPassword: true,
+//       },
+//     });
+//     if (!user) throw new NotFoundException('No user found');
+//     const isCorrectPassword = compare(user.encryptedPassword, password);
+//     if (!isCorrectPassword) throw new ForbiddenException('Incorrect password');
+//     const accessToken = this.generateAccessToken(user);
+//     return accessToken;
+//   }
+
+//   async generateAccessToken(user: Pick<User, 'id' | 'email'>) {
+//     const { id: subject, email } = user;
+//     const secretKey = this.configService.getOrThrow<string>('JWT_SECRET_KEY');
+//     const accessToken = sign({ email }, secretKey, {
+//       subject,
+//       expiresIn: '2h',
+//     });
+//     return accessToken;
+//   }
+// }
+
 import {
   ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Prisma, User } from '@prisma/client';
 import { compare, hash } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { PrismaService } from 'src/db/prisma/prisma.service';
@@ -17,45 +79,73 @@ export class AuthService {
     private readonly prismaService: PrismaService,
     private readonly configService: ConfigService,
   ) {}
+
   async signUp(dto: AuthDto) {
     const { email, password } = dto;
-    const data: Prisma.UserCreateInput = {
-      id: generateRandomId(),
-      email,
-      encryptedPassword: await hash(password, 12),
-    };
+    const encryptedPassword = await hash(password, 12);
     const user = await this.prismaService.user.create({
-      data,
-      select: { id: true, email: true },
+      data: {
+        id: generateRandomId(),
+        email,
+        encryptedPassword,
+      },
     });
-    const accessToken = this.generateAccessToken(user);
-    return accessToken;
+    const accessToken = this.generateAccessToken({
+      id: user.id,
+      email: user.email,
+    });
+
+    return { accessToken };
   }
 
   async logIn(dto: AuthDto) {
     const { email, password } = dto;
     const user = await this.prismaService.user.findUnique({
       where: { email },
-      select: {
-        id: true,
-        email: true,
-        encryptedPassword: true,
-      },
     });
     if (!user) throw new NotFoundException('No user found');
-    const isCorrectPassword = compare(user.encryptedPassword, password);
+    const isCorrectPassword = await compare(password, user.encryptedPassword);
     if (!isCorrectPassword) throw new ForbiddenException('Incorrect password');
-    const accessToken = this.generateAccessToken(user);
-    return accessToken;
+    const accessToken = this.generateAccessToken({
+      id: user.id,
+      email: user.email,
+    });
+    // this.setAccessTokenCookie(response, accessToken);
+
+    return { accessToken };
   }
 
-  async generateAccessToken(user: Pick<User, 'id' | 'email'>) {
-    const { id: subject, email } = user;
-    const secretKey = this.configService.getOrThrow<string>('JWT_SECRET_KEY');
-    const accessToken = sign({ email }, secretKey, {
-      subject,
+  // async logOut() {
+
+  //   // return { message: 'Logged out successfully' };
+  // }
+
+  // async refreshAccessToken(user: User, response: Response) {
+  //   // const user = request.user;
+  //   if (!user) {
+  //     this.logOut(response);
+  //     return false;
+  //   }
+  //   const accessToken = await this.generateAccessToken(user);
+
+  //   this.setAccessTokenCookie(response, accessToken);
+  //   return true;
+  // }
+
+  private generateAccessToken(user: { id: string; email: string }) {
+    const secretKey = this.configService.get<string>('JWT_SECRET_KEY');
+    const accessToken = sign({ email: user.email }, secretKey, {
+      subject: user.id,
       expiresIn: '2h',
     });
     return accessToken;
   }
+
+  // private setAccessTokenCookie(response: Response, accessToken: string) {
+  //   response.cookie('accessToken', accessToken, {
+  //     secure: true,
+  //     sameSite: 'none',
+  //     maxAge: 1000 * 60 * 60 * 2,
+  //   });
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/httpException.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(5050);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin === 'http://localhost:3000') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(5050);
 }
 bootstrap();
